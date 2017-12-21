4dd8f988db3a1f90c412ac3913529bc54c18be99
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-12-21 19:15:55 +0000

    DFS using recursion.

--- src/com/mounacheikhna/graph/dfs.py
+++ src/com/mounacheikhna/graph/dfs.py
@@ -8,6 +8,16 @@ def dfs_stack(graph, start):
             stack.extend(graph[vertex] - visited)
     return visited
 
+
+def dfs_recur(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for n in graph[start] - visited:
+        dfs_recur(graph, n, visited)
+    return visited
+
+
 g = {'A': {'B', 'C'},
      'B': {'A', 'D', 'E'},
      'C': {'A', 'F'},
@@ -16,3 +26,4 @@ g = {'A': {'B', 'C'},
      'F': {'C', 'E'}}
 
 print dfs_stack(g, 'A')
+print dfs_recur(g, 'A')
